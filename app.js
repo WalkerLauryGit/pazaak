81efa56ac44703111990605dad72976a5e7daee3
Walker Laury <walkerclaury@protonmail.com>
2020-04-13 11:15:55 -0500

    created the playcard function.  Removes the original card, and adds a new card.

--- app.js
+++ app.js
@@ -21,12 +21,43 @@ function loadHand(){
     let div = document.createElement('div');
     div.classList.add('shadow');
     div.classList.add('zoom');
-    
+    div.setAttribute('id', i);
+    div.setAttribute('value', hand[i])
+    div.id = i;
+    div.setAttribute('onclick', 'playCard(this.id)')
     let content = document.createTextNode(hand[i]);
     div.appendChild(content);
 
     handOfCards.appendChild(div);
   }
+}
 
+function playCard(cardId){
+
+  let board = document.getElementById('player-one');
+  card = document.getElementById(cardId);
+  console.log(card);
+  let value = card.getAttribute('value');
+
+  let div = document.createElement('div');
+  div.classList.add('board-slot');
+  // div.setAttribute('id', cardId);
+  div.setAttribute('value', value);
+  let content = document.createTextNode(value);
+  div.appendChild(content);
+
+  //Delete the original
+  document.getElementById(cardId).remove();
+
+  board.appendChild(div);
+  
+  
+
+  //1. get the id in card.id -X-
+  //2. grab the inner text of card with the id -X-
+  //3. delete the original card from the hand
+  //4. create a new card
+  //5. add new card to the board
+  //6. update the total points
 
 }
